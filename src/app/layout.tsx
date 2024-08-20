66eb6e64b569bffc423382e7f1e072add8cbf3a9
Marty <86281023+esot321c@users.noreply.github.com>
2024-08-19 20:32:57 -0700

    Test preview (#1)
    * updating config and databases
    
    * test
    
    * fix error no-empty-object-type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "~/styles/globals.css";
+import { ThemeProvider } from "~/components/theme-provider"
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
@@ -17,7 +18,15 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <TRPCReactProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >{children}
+          </ThemeProvider>
+        </TRPCReactProvider>
       </body>
     </html>
   );
